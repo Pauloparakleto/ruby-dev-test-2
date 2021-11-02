90974a3380a3c1828fda2643330c5777b93a795e
paulo-felipe <alparakleto@gmail.com>
2021-11-02 16:38:40 -0300

    refactor(spec/models/player_spec.rb): reduce block length to solve offenses

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -3,35 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
+  let!(:player) { described_class.create(FactoryBot.attributes_for(:player)) }
+  let!(:name) { 'Madonna' }
+
   context 'when create' do
     it 'has name' do
-      name = 'Madonna'
-      player = described_class.create(FactoryBot.attributes_for(:player))
-
       expect(player.name).to eq(name)
     end
 
     it 'has invalid nil name' do
-      player = described_class.new(name: nil)
-
-      expect(player.valid?).to be_falsey
+      expect(described_class.new(name: nil).valid?).to be_falsey
     end
   end
 
   context 'when update' do
-    let!(:player) { described_class.create(FactoryBot.attributes_for(:player)) }
-
     it 'has name' do
-      name = 'Shakira'
-      player.update(name: name)
+      player.update(name: 'Shakira')
 
-      expect(player.reload.name).to eq(name)
-    end
-
-    it 'has invalid nil name' do
-      player.update(name: nil)
-
-      expect(player.valid?).to be_falsey
+      expect(player.reload.name).to eq('Shakira')
     end
   end
 
