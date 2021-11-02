eff036bc2c0c785c51e5861ca8331ff9a392b4a0
paulo-felipe <alparakleto@gmail.com>
2021-11-02 15:30:38 -0300

    test(spec/models/player_spec.rb): add when update with nil name

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe Player, type: :model do
       expect(player.valid?).to be_falsey
     end
   end
+
+  context 'when update' do
+    it 'has name' do
+      name = 'Shakira'
+      player = described_class.create(FactoryBot.attributes_for(:player))
+      player.update(name: name)
+
+      expect(player.reload.name).to eq(name)
+    end
+
+    it 'has invalid nil name' do
+      player = described_class.create(FactoryBot.attributes_for(:player))
+      player.update(name: nil)
+
+      expect(player.valid?).to be_falsey
+    end
+  end
 end
